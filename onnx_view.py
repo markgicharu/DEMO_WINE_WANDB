a0aeff225ffb0e9bcb43d4a81273250c324e63fd
MARK GICHARU <16897706+markgicharu@users.noreply.github.com>
2021-07-16 20:05:27 +0300

    test run of onnx

--- onnx_view.py
+++ onnx_view.py
@@ -1,6 +1,6 @@
 import onnx
 import onnxruntime
 
-model = onnx.load("models/fashion_mnist_{t}.onnx")
+model = onnx.load("models/home_state_test.onnx")
 onnx.checker.check_model(model)
 print(onnx.helper.printable_graph(model.graph))
